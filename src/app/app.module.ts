fffc79c521b5b561d6409a05803e22f9300b79d5
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-05-31 17:35:04 +0300

    changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import {UsersComponent} from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
