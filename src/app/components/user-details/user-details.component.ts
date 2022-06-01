5481d0f77d94b4c167cc32fa7726b920636a283e
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-06-01 18:19:51 +0300

    lesson2 ddos

---
+++ src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../../services/user.service";
+import {IUser} from "../../models/IUser";
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent implements OnInit {
+
+  userDetailsObj:IUser;
+
+  constructor(private activatedRoute:ActivatedRoute,
+              private userService:UserService) { }
+
+  ngOnInit(): void {
+    // this.activatedRoute.url.subscribe(value => console.log(value))
+    // this.activatedRoute.params.subscribe(value => console.log(value))
+    this.activatedRoute.params.subscribe(({id}) =>{
+      this.userService.getUser(id).subscribe(value => this.userDetailsObj=value)
+    })
+  }
+
+}
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/models/IUser';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  users: IUser[];
+  constructor(private userService:UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(value=>this.users=value)
+  }
+
+}
