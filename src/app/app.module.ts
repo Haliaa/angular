61ef9870a222a3b0a079c187ed9e64d699d8894d
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-06-01 16:40:05 +0300

    lesson2 base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'home-page', component:HomeComponent},
+      {path:'users-page',
+        component:UsersComponent,
+        children:[
+          {path:'user-details/:id', component:UserDetailsComponent}
+        ]
+      },
+      // {path:'user-details/:id', component:UserDetailsComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
