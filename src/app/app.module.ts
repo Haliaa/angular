0dcf74fce8c1cc2b5708ae8d53d8bdeaea3bf5b0
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-06-04 15:39:51 +0300

    PIPES |json, upper, lover, custom, async...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { CustomPipe } from './services/pipes/custom.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomPipe
   ],
   imports: [
     BrowserModule,
