8f3ae136775ac5d78359254f2438fd5f453ad03e
Haliaa <halyna.shevchyk.pm.2020@lpnu.ua>
2022-06-04 13:34:47 +0300

    emitt

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {IUser} from "../../models/IUser";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  @Input()
+  user:IUser;
+
+  @Output()
+  userEmitter=new EventEmitter<IUser>()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  emitt():void {
+    this.userEmitter.emit(this.user)
+  }
+}
